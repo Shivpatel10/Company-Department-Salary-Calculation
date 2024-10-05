8ede9c2386ec52dbfa187d218cd694b96821cb32
Shiv Patel <shivpatel0204@gmail.com>
2024-10-04 22:28:07 -0400

    Calculate total salary for all departments in the company

--- company_department_salary_calculation.js
+++ company_department_salary_calculation.js
@@ -55,12 +55,25 @@ function calculateDepartmentSalary(department) {
         totalDepSalary += employee.salary;
 
         
-        for (let subordinate of employee.subordinates) {  // If the employee has subordinates, recursively calculate their salaries
+     for (let subordinate of employee.subordinates) {  // If the employee has subordinates, recursively calculate their salaries
             totalDepSalary += calculateDepartmentSalary({ employees: [subordinate] });
         }}
     return totalDepSalary;
 };
 
-// Calculate total salary for the Operations department
 console.log(`Total Salary for Chris's Department: $${calculateDepartmentSalary({ employees: [company.departments[0].employees[0]] })}`);
-// ^^ This console.log will return Chris's Departments Total Salary.
+// ^^ This console.log will return Chris's departments total salary.
+
+
+// Task 3: Create a Function to Calculate the Total Salary for All Departments
+function calculateCompanySalary(company) {
+    let totalCompanySalary = 0;
+
+    for (let department of company.departments) { // Loop through each department in the company
+        totalCompanySalary += calculateDepartmentSalary(department);
+    }
+return totalCompanySalary;
+};
+
+console.log(`Total Salary for the Entire Company: $${calculateCompanySalary(company)}`);
+// ^^ This console.log will return the companies total salary.
